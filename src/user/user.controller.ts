8c7e01c6a2611748a2810cd1cb22c531f96ef6e4
andriy-shpontak <andriishpontak@gmail.com>
2024-09-08 09:05:16 +0300

    fix user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,23 +1,19 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { InjectModel } from '@nestjs/sequelize';
-import { User } from './models/user.model';
+import { UserService } from './user.service';
 
 @Controller('api/v1')
 export class UserController {
-  constructor(
-    @InjectModel(User)
-    private userModel: typeof User,
-  ) {}
+  constructor(private userService: UserService) {}
 
   @Post('add-user')
   async addUser(@Body() body: { name: string; email: string; phone: string }) {
-    const user = await this.userModel.create(body);
+    const user = await this.userService.addUser(body);
     console.log('User added:', user);
     return user;
   }
 
   @Get('get-user/:id')
   async getUser(@Param('id') id: number) {
-    return await this.userModel.findByPk(id);
+    return await this.userService.getUser(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './models/user.model';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectModel(User)
+    private userModel: typeof User,
+  ) {}
+
+  async addUser(body: { name: string; email: string; phone: string }) {
+    return await this.userModel.create(body);
+  }
+
+  async getUser(id: number) {
+    return await this.userModel.findByPk(id);
+  }
+}
